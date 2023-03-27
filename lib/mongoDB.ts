93a226f137e3a996bfd23509faa145f8b8c24649
diep4s <diep.le@0004s.com>
2023-03-27 17:05:00 +0900

    update

---
+++ lib/mongoDB.ts
@@ -0,0 +1,16 @@
+import { MongoClient, MongoClientOptions } from "mongodb";
+
+if (!process.env.MONGODB_USER || !process.env.MONGODB_PASS) {
+  throw new Error("Please add your Mongo URI to .env.local");
+}
+const options: MongoClientOptions = {};
+const username = encodeURIComponent(process.env.MONGODB_USER);
+const password = encodeURIComponent(process.env.MONGODB_PASS);
+const uri = `mongodb+srv://${username}:${password}@main.g1zf50u.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, options);
+const database = client.db("main");
+
+export const usersDB = database.collection<{
+  name: string;
+}>("users");
---
+++ pages/api/user/index.ts
@@ -0,0 +1,19 @@
+import { usersDB } from "@/lib/mongoDB";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const users = await usersDB.find().toArray();
+    res.send(users);
+  } catch (error) {
+    console.log(error);
+    res.status(Number(error) || 500).end();
+  }
+}
